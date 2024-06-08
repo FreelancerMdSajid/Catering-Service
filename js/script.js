332a2b9f9f8fac5e9aa4b5329a8fc3cc4e1f34cb
FreelancerMdSajid <freelancermdsajid@gmail.com>
2024-06-08 19:58:37 +0600

    full

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
+// FAQ
 var faqQuestion = document.getElementsByClassName("faqQuestion");
 var i;
 for (i = 0; i < faqQuestion.length; i++) {
   faqQuestion[i].addEventListener("click", function () {
     this.classList.toggle("activefaq");
-    var faqAnswer = this.nextElementSibling;
-    if () {
 
+    var faqAnswer = this.nextElementSibling;
+    if (faqAnswer.style.maxHeight) {
+      faqAnswer.style.maxHeight = null;
     } else {
-      
+      faqAnswer.style.maxHeight = faqAnswer.scrollHeight + "px";
     }
   });
 }
 
+// Show Time
+function timeShow() {
+  setInterval(function () {
+    var today = new Date(),
+      h = today.getHours(),
+      m = today.getMinutes(),
+      s = today.getSeconds();
+    let time = "Current Time = " + h + ":" + m + ":" + s;
+    document.getElementById("timeShow").innerHTML = time;
+  }, 1000);
+}
 
+// function date() {
+//     setInterval(function() {
+//         document.getElementById("text").innerHTML = new Date().toString();
 
+//     },1000);
+// }
+
+// Contact Form  Submit Alert
+function alertMassage() {
+  confirm("You Have Submitted Successfully");
+}
